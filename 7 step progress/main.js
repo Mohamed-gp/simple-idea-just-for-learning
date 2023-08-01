1c07f203c9aefe033b6d26673e1e6a2b1484e091
Mohamed-gp <mohamedterba6@gmail.com>
2023-08-01 23:53:23 +0100

    not completed yet

--- 7 step progress/main.js
+++ 7 step progress/main.js
@@ -1,62 +1,75 @@
-// var box      = document.getElementById("box");
-// var boxAfter = window.getComputedStyle(box, "::after");
-// console.log(boxAfter.backgroundColor);// getting the background color
 let right = document.querySelector(".right");
 let left = document.querySelector(".left");
-let progress = document.querySelector(".progress");
 let before = document.querySelector(".before");
-let after = document.querySelector(".after");
 let spans = document.querySelectorAll("span");
-console.log(left);
+let skills = document.querySelectorAll(".progress p:first-of-type");
+let arrow = document.querySelectorAll(".progress p:last-of-type");
+let congratulation = document.querySelector(".congratulations")
 let i = 0;
+
 before.style.width = "0";
 right.onclick = function () {
-  i++;
-
-  if (i >= 1) {
+  if (parseInt(before.style.width) == 0) {
+    i++;
     left.style.opacity = "1";
-  } else {
-    left.style.opacity = ".5";
+    before.style.width = `${parseInt(before.style.width) + 25}%`;
+    spans[i].children[0].remove();
+    let iElement = document.createElement("i");
+    iElement.className = "fa-solid fa-check fa-fw";
+    iElement.style.color = "green";
+    spans[i].prepend(iElement);
+    spans[i].style.borderColor = "green";
+    left.style.cursor = "pointer";
+    skills[i].style.backgroundColor = "green";
+    arrow[i].style.borderColor = "green transparent transparent transparent";
+  } else if (
+    parseInt(before.style.width) > 0 &&
+    parseInt(before.style.width) < 100
+  ) {
+    i++;
+    before.style.width = `${parseInt(before.style.width) + 25}%`;
+    spans[i].children[0].remove();
+    let iElement = document.createElement("i");
+    iElement.className = "fa-solid fa-check fa-fw";
+    iElement.style.color = "green";
+    spans[i].prepend(iElement);
+    skills[i].style.backgroundColor = "green";
+    arrow[i].style.borderColor = "green transparent transparent transparent";
+    spans[i].style.borderColor = "green";
+    if (parseInt(before.style.width) == 100) {
+      right.style.opacity = ".5";
+      right.style.cursor = "unset";
+      skills[i].style.backgroundColor = "green";
+      arrow[i].style.borderColor = "green transparent transparent transparent";
+
+    }
   }
-  if (i >= 4) {
-    right.style.opacity = ".5";
-  } else {
-    right.style.opacity = "1";
-  }
-  if(i >= 1 && i <= 4){
-      before.style.width = `${parseInt(before.style.width) + 25}%`
-      
+  if (parseInt(before.style.width) == 100) {
+    congratulation.style.display = "block"
+    document.querySelector(".title").style.marginBottom = "20px"
   }
 
-  //   else if(){
+};
 
-  //   }
-  //   if (i >= 1) {
-  //     left.style = "opacity : 1 !important";
-  //     left.style.cursor = "pointer";
-  //     right.style.opacity = "1";
-  //     left.onclick = function () {
-  //       before.style.width = `${parseInt(before.style.width) - 25}%`;
-  //       i--;
-  //     };
-  //   }
-  //   if (i <= 4) {
-  //     right.style.opacity = "1";
-  //     let iElement = document.createElement("i");
-  //     iElement.className = "fa-solid fa-check fa-fw";
-  //     spans[i].children[0].remove();
-  //     spans[i].append(iElement);
-  //     spans[i].children[0].style.color = "green";
-  //     if (i == 4) {
-  //       right.style.cursor = "unset";
-  //       before.style.width = `${parseInt(before.style.width) + 25}%`;
-  //       spans[i].style.borderColor = "green";
-  //       spans[i].style.color = "green";
-  //     } else {
-  //       before.style.width = `${parseInt(before.style.width) + 25}%`;
-  //       spans[i].style.borderColor = "green";
-  //     }
-  //   } else {
-  //     i--;
-  //   }
+left.onclick = function () {
+  if (parseInt(before.style.width) > 0 && parseInt(before.style.width) <= 100) {
+    before.style.width = `${parseInt(before.style.width) - 25}%`;
+    spans[i].children[0].remove();
+    let iElement = document.createElement("i");
+    iElement.className = "fa-solid fa-xmark fa-fw";
+    spans[i].prepend(iElement);
+    skills[i].style.backgroundColor = "rgba(159, 210, 255, 0.493)";
+    arrow[i].style.borderColor = "rgba(159, 210, 255, 0.493) transparent transparent transparent";
+    spans[i].style.borderColor = "rgba(159, 210, 255, 0.493)";
+    i--;
+    if (parseInt(before.style.width) == 0) {
+      left.style.opacity = ".5";
+      left.style.cursor = "unset";
+      right.style.opacity = "1";
+      right.style.cursor = "pointer";
+    }
+  }
+  if (parseInt(before.style.width) <= 100) {
+    congratulation.style.display = "none"
+  }
 };
